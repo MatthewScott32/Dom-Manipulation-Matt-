5f62e60faf95642d82434bbe605146a52c116915
Matthew Blagg <matthewscottblagg@gmail.com>
2019-10-10 11:32:03 -0500

    finished exercise

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,6 +5,16 @@ headerOne.textContent = 'Welcome to the Matt blog';
 console.log(headerOne);
 
 const headerTwo = document.querySelectorAll(".article__header");
-headerTwo.classList.remove(".article__header");
-headerTwo.classList.add(".article__header important");
 console.log(headerTwo);
+
+for (let i = 0; i < headerTwo.length; i++) {
+    headerTwo[i].classList.add("important");
+  }
+
+const classDashed = document.querySelector(".dashed")
+classDashed.classList.remove("dashed")
+console.log(classDashed);
+
+const classGoldenRod = document.querySelector('.article__footer')
+classGoldenRod.classList.add('goldenrod');
+console.log(classGoldenRod);
